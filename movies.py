40fc6502135f09ea0d8392a37c865ee1f28a527d
gparuthi <gparuthi@umich.edu>
2013-01-31 01:28:29 -0500

    added try catch

--- movies.py
+++ movies.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 ifile  = open('zips.csv', "rU")
 reader = csv.reader(ifile) 
-output_filep = './output_' + str(datetime.now()) + '.json' 
+output_filep = 'logs/output_' + str(datetime.now()) + '.json' 
 logo = logger('log')
 
 logo.log('Output file initiated at: '+ output_filep)
@@ -18,13 +18,16 @@ for row in reader:
 	logo.log( '[%d] %s' %(c,row))
 	zip = int(row[0])
 	c+=1
-	
-# zip = 48103
-	r = requests.get('http://data.tmsapi.com/v1/movies/showings?startDate=2013-01-30&zip='+str(zip)+'&radius=5&api_key=pbsxwvjascgj72kd6rffxpxv')
-	r.status_code
-	j = r.json()
+	try:
+	# zip = 48103
+		r = requests.get('http://data.tmsapi.com/v1/movies/showings?startDate=2013-01-30&zip='+str(zip)+'&radius=5&api_key=pbsxwvjascgj72kd6rffxpxv')
+		r.status_code
 
-	res[zip] = j
+		j = r.json()
+
+		res[zip] = j
+	except:
+		logo.log('Error with zip code ' + row)
 
 # logo.log( res
 
