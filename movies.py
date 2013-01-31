871b3bd49266b0586630ddffdc4d273d997e570a
gparuthi <gparuthi@umich.edu>
2013-01-31 00:37:41 -0500

    added status

--- movies.py
+++ movies.py
@@ -7,21 +7,22 @@ ifile  = open('zips.csv', "rb")
 reader = csv.reader(ifile)
 
 res = {}
-
+c = 1
 for row in reader:
-	print row
+	print '[%d] %s' %(c,row)
 	zip = int(row[0])
+	c+=1
 	
 # zip = 48103
-	r = requests.get('http://data.tmsapi.com/v1/movies/showings?startDate=2013-01-23&zip='+str(zip)+'&radius=100&api_key=pbsxwvjascgj72kd6rffxpxv')
+	r = requests.get('http://data.tmsapi.com/v1/movies/showings?startDate=2013-01-30&zip='+str(zip)+'&radius=100&api_key=pbsxwvjascgj72kd6rffxpxv')
 	r.status_code
 	j = r.json()
 
 	res[zip] = j
 
-print res
+# print res
+
 for z in res:
-	print str(z) + '---'
 	for m in res[z]:
 		print m['title']
 
